cbb8a2f1afe92be10d2e01f4e6afefc1389b8820
franciscoj <frankmazta@gmail.com>
2025-05-16 12:16:39 -0400

    Eliminar el componente App y su lógica de contador

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+// import { Form } from "./form/form.js";
+// import { List } from "./list/List.js";
+// import { Presentation } from './presentations/Presentation.js'
+// import { Count } from  "./contador/Count.js"
+ 
+// export const APP = () => {
+//  return (
+//     <>
+//       <Presentation />
+//       <Form value={currentTask} onChange={setCurrentTask} onAddTask={handleAddTask} />
+//       <List tasks={tasks} onDeleteTask={handleDeleteTask} />
+//       <Count/>
+//     </>
+//   );
+// }
