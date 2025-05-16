ceb39a267bb8997b5f73bc16151097992dc4b9da
franciscoj <frankmazta@gmail.com>
2025-05-16 12:16:53 -0400

    Agregar tipos de TypeScript al componente Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,15 @@
 import "./button.css"
 
-export const Button = ({ label, parentMethod }) => {
+interface buttonProps{
+  label: string;
+  parentMethod: ()=> void
+}
+
+export const Button = ({ label, parentMethod }: buttonProps) => {
   return (
     <button onClick={parentMethod} className="btn">
       {label}
     </button>
   );
 };
+
