b694445fb6034bc44e0a4c7fdb69e6a59b47bf9d
franciscoj <frankmazta@gmail.com>
2025-05-16 12:37:35 -0400

    Restaurar el componente App en lugar de TaskApp en el punto de entrada

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TaskApp } from './components/TaskApp.tsx'
+import { App } from './components/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TaskApp/>
+    <App/>
   </StrictMode>,
 )
