48ead8edb0b2828be9e07c336d9482ced893c820
franciscoj <frankmazta@gmail.com>
2025-05-16 12:37:25 -0400

    Restaurar el componente App y su lógica asociada

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,31 @@
-// import { Form } from "./form/form.js";
-// import { List } from "./list/List.js";
-// import { Presentation } from './presentations/Presentation.js'
-// import { Count } from  "./contador/Count.js"
+import { Form } from "./form/form.js";
+import { List } from "./list/List.js";
+import { Presentation } from './presentations/Presentation.js'
+import { Count } from  "./contador/Count.js"
+import { useTasks } from "./UseTasks.js";
  
-// export const APP = () => {
-//  return (
-//     <>
-//       <Presentation />
-//       <Form value={currentTask} onChange={setCurrentTask} onAddTask={handleAddTask} />
-//       <List tasks={tasks} onDeleteTask={handleDeleteTask} />
-//       <Count/>
-//     </>
-//   );
-// }
+export const App = () => {
+  const {
+    tasks,
+    currentTask,
+    setCurrentTask,
+    handleAddTask,
+    handleDeleteTask
+  } = useTasks();
+
+  return (
+    <>
+      <Presentation />
+      <Form 
+        value={currentTask} 
+        onChange={setCurrentTask} 
+        onAddTask={handleAddTask} 
+      />
+      <List 
+        tasks={tasks} 
+        onDeleteTask={handleDeleteTask} 
+      />
+      <Count />
+    </>
+  );
+};
