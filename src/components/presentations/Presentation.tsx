8322f8fb0ab7c5585d0ffc669d561391c8fcc716
franciscoj <frankmazta@gmail.com>
2025-06-01 12:48:47 -0400

    Agregar componente Presentation con usuarios y sus propiedades

--- src/components/presentations/Presentation.tsx
+++ src/components/presentations/Presentation.tsx
@@ -1,9 +1,14 @@
 import './presentation.css'
+import { User } from '../user/User.tsx'
+
 
 export const Presentation =()=> {
   return (
     <header>
       <h1>curso comenzado por frank</h1>
+      <User name= 'frank' age= {35} country = 'R.Dominicana'/>
+      <User name= 'ana' age= {32} country = 'Haiti'/>
+      <User name= 'ales' age= {45} country = 'R.Dominicana'/>
     </header>
   )
 }
